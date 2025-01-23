6ea14901b4fc0bd71b2f70640bb74172e4014502
김윤혁 <kyhyeok@gmail.com>
2025-01-23 23:09:50 +0900

    Final Variables

--- bin/dart_start.dart
+++ bin/dart_start.dart
@@ -15,7 +15,7 @@ void main(List<String> arguments) {
   // dynamic 타입은 다양한 타입을 가질 수 있기 때문에 반드시 사용해야 할 경우에만 사용 권장
 
   //dart의 변수타입은 기본적으로 nullable하다.
-  //null safety - 개발자가 null 값을 참조할 수 없도록 한다.
+  //null safety - 개발자가 null 값을 참조할 수 없도록 한다
   String name5 = 'kim';
   // name5 = null; null이 될 수 없음
 
@@ -24,5 +24,8 @@ void main(List<String> arguments) {
   // name6.isNotEmpty; null에는 isNotEmpty가 없기 때문에 에러
   // name6?.isNotEmpty; if(name6 != null) { name6.isNotEmpty }
 
+  //Final Variables
+  final name7 = 'kim'; // final은 변수가 수정할 수 없도록 해준다 (상수)
+  final String name8 = 'kim'; // type 추가가 가능하다
 
 }
