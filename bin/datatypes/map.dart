bdfcc44e140ed007288459f886d2fe6d127c75b6
김윤혁 <kyhyeok@gmail.com>
2025-01-24 10:24:58 +0900

    DataTypes-Maps

---
+++ bin/datatypes/map.dart
@@ -0,0 +1,17 @@
+void main() {
+  var player = {
+    'name': 'kim',
+    'xp': 19.99,
+    'superPower': false,
+  };
+  //var를 사용할 때는 자료형을 따로 명시해줄 필요가 없다.
+  //그 이유는 컴파일러가 자료형을 유추해서 정해주기 때문이다.
+
+  Map<int, bool> player2 = {
+    1: true,
+    2: false,
+    3: true,
+  };
+
+
+}
