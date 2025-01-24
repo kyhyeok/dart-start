2af7034ea165b23967a9232ad3768c2880991281
김윤혁 <kyhyeok@gmail.com>
2025-01-24 13:40:00 +0900

    Classes - Named Constuctors

---
+++ bin/classes/named_constructors.dart
@@ -0,0 +1,33 @@
+class Player {
+  final String name;
+  int xp, age;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age
+  });
+
+  Player.createBluePlayer({required String name, required int age,
+  }) :  this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age,
+  ) :  this.age = age,
+       this.name = name,
+       this.team = 'red',
+       this.xp = 0;
+}
+
+void main() {
+  var player = Player.createBluePlayer(
+    name: 'kim',
+    age: 100,
+  );
+
+  var player2 = Player.createRedPlayer('kim', 100,);
+}
